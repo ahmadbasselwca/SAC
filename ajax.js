d16f050607f59c59e738a2b27d83b13f7febe7fc
ahmadbasselwca <ahmadbassel.alshalah@walldorfconsulting.com>
2025-01-30 11:07:09 +0100

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,11 +1,13 @@
+const url = "https://api.openai.com/v1";
+
 (function () {
     class MainWebComponent extends HTMLElement {
         async post(apiKey, endpoint, prompt) {
-            var response = apiKey + ' => ' + `${endpoint}` + ' => ' + prompt;
+            var response = apiKey + ' => ' + `${url}/${endpoint}` + ' => ' + prompt;
             console.log(["response", response]);
             return response;
             }
         }
     
-    customElements.define("script-element", MainWebComponent);
+    customElements.define("custom-widget", MainWebComponent);
 })();
