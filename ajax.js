f5dbd56aea42e8ac738822b0d70fcaf61d530d48
ahmadbasselwca <ahmadbassel.alshalah@walldorfconsulting.com>
2025-01-30 10:50:19 +0100

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,17 +1,15 @@
-(function () {
-    const template = document.createElement("template");
-    template.innerHTML = `
-        <style>
-        </style>
-        <div id="root" style="width: 100%; height: 100%;">
-        </div>
-      `;
-    class MainWebComponent extends HTMLElement {
-      async post(apiKey, endpoint, prompt) {
-        const response = apiKey + ' => ' + `${endpoint}` + ' => ' + prompt;
-        console.log(response);
-        return response;
-      }
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+    </style>
+    <div id="root" style="width: 100%; height: 100%;">
+    </div>
+    `;
+class MainWebComponent extends HTMLElement {
+    async post(apiKey, endpoint, prompt) {
+    const { response } = apiKey + ' => ' + `${endpoint}` + ' => ' + prompt;
+    console.log(response);
+    return response;
     }
-    customElements.define("custom-widget", MainWebComponent);
-  })();
+}
+customElements.define("custom-widget", MainWebComponent);
